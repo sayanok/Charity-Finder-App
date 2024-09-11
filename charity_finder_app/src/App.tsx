9405d49d1001618d54aa370de6d1d09de090cce3
sayanok <rng.jhn.osca@gmail.com>
2024-09-11 15:59:14 +0900

    add form作成途中まで

--- charity_finder_app/src/App.tsx
+++ charity_finder_app/src/App.tsx
@@ -1,9 +1,26 @@
-import React from "react";
+import { FormControl, InputLabel, MenuItem, Select } from "@mui/material";
+import React, { useState } from "react";
 
 const App: React.FC = () => {
+  const [organizationName, setOrganizationName] = useState<Array<string>>();
+
   return (
     <>
       <div>組織名：</div>
+      <FormControl fullWidth>
+        <InputLabel id="demo-simple-select-label">Age</InputLabel>
+        <Select
+          labelId="demo-simple-select-label"
+          id="demo-simple-select"
+          value={[1, 2, 3]}
+          label="Age"
+          //onChange={onChangeHandler()}
+        >
+          <MenuItem value={10}>Ten</MenuItem>
+          <MenuItem value={20}>Twenty</MenuItem>
+          <MenuItem value={30}>Thirty</MenuItem>
+        </Select>
+      </FormControl>
       <div>組織の母国：</div>
       <div>組織がサービスを提供している国：</div>
     </>
